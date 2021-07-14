1fcc6faaca8cdfd4eef47d7b36829133b73dff08
Junior Souza <joaobat1999@gmail.com>
2021-07-14 12:02:37 -0300

    feat: create routes

--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,14 @@
-const routes = require('express').Router();
+const routes = require("express").Router();
+
+const authMiddleware = require("./app/middleware/auth");
+const SessionController = require("../src/app/controllers/session.controller");
+
+routes.post("/sessions", SessionController.store);
+
+routes.use(authMiddleware);
+
+routes.get("/dashboard", (req, res) => {
+  return res.status(200).send();
+});
 
 module.exports = routes;
