ef8b3832aa5e7f5d9fc0425a132fd242ccb47417
Junior Souza <joaobat1999@gmail.com>
2021-07-14 12:02:21 -0300

    feat: create database config

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,15 @@
+require('dotenv').config({
+  path: process.env.NODE_ENV === "test" ? '.env.test' : '.env'
+})
+
 module.exports = {
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   host: process.env.DB_URL,
   port: process.env.DB_PORT,
-  dialect: "postgres",
+  dialect: process.env.DB_DIALECT || "postgres",
+  storage: './__tests__/database.sqlite',
   operatorsAliases: 1,
   logging: false,
   define: {
